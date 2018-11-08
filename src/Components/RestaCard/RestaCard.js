c5b76f5923e587f7a8d3c88637b96d8e0e8bc5da
paulvvo <paulvo@live.ca>
2018-11-07 20:52:25 -0500

    added more styling to restacard components

--- src/Components/RestaCard/RestaCard.js
+++ src/Components/RestaCard/RestaCard.js
@@ -3,11 +3,15 @@ import React from "react";
 
 const RestaCard = (props) =>{
 	const {restaurant}  = props;
+	console.log(restaurant);
 	return(
-		<div>
-			{
-				restaurant.name
-			}
+		<div className="restaCard">
+
+				<div className="icon"><i className="fas fa-utensils"></i></div>
+				<h4>{restaurant.name}</h4>
+				<img src={restaurant.image_url} alt="#0"/>
+				<p> Location: {restaurant.address} </p>
+				<p> Price:{restaurant.price}$ </p>
 		</div>
 	)
 }
--- src/Components/RestaCardContainer/RestaCardContainer.js
+++ src/Components/RestaCardContainer/RestaCardContainer.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "./RestaCardContainer.css";
 //Components
 import RestaCard from "../RestaCard/RestaCard";
 
@@ -7,7 +7,7 @@ const RestaCardContainer = (props) =>{
 	// console.log(props.restaurantList);
 	// console.log(typeof props.restaurantList);
 	return(
-		<div>
+		<div className="restaCardContainer">
 			{
 				props.restaurantList.map((item) =>{
 					return <RestaCard key={item.id} restaurant={item}/>
