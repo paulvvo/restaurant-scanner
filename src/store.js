526b91aa5e2c01adc4baf352b3b6beadb2d7e06b
paulvvo <paulvo@live.ca>
2018-12-16 21:38:38 -0500

    created store and apple middleware and rootreducers

--- src/store.js
+++ src/store.js
@@ -1,4 +1,18 @@
 import thunk from "redux-thunk";
 import {applyMiddleware, createStore, compose} from "redux";
 
+//reducer
+import RootReducer from "./Reducers/RootReducer";
+
 const initialState={};
+
+
+const store = createStore(
+	RootReducer,
+	initialState,
+	compose(
+		applyMiddleware(thunk),
+		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	));
+
+export default store;
